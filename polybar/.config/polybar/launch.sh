aac515fa87c6c1c61f2cdbaac66aacb8d1ca8c14
GwakaMolie <bazme99@gmail.com>
2021-11-07 06:11:05 +0100

    modules in polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar mybar -r &
-echo "Bars launched..."
+export ETH_INTERFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1)
+export WLAN_INTERFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1)
+
+# polybar main -r &
+polybar mainBar -c $HOME/.config/polybar/config.ini &
